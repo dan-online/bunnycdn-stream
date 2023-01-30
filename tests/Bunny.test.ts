b397494e4c7f30dce139acf08448c9665a40dbb5
DanCodes <dan@dancodes.online>
2023-01-30 16:11:51 +0000

    tests: switch to vitest

--- tests/Bunny.test.ts
+++ tests/Bunny.test.ts
@@ -1,23 +1,26 @@
+import { describe, expect, test } from 'vitest';
 import { BunnyCdnStream } from '../src';
+describe(
+  'BunnyCdnStream',
+  () => {
+    let stream: BunnyCdnStream;
 
-describe('BunnyCdnStream', () => {
-  let stream: BunnyCdnStream;
+    describe('is a class', () => {
+      test('GIVEN typeof BunnyCdnStream THEN returns function', () => {
+        expect(typeof BunnyCdnStream).toBe('function');
+      });
 
-  describe('is a class', () => {
-    test('GIVEN typeof BunnyCdnStream THEN returns function', () => {
-      expect(typeof BunnyCdnStream).toBe('function');
+      test('GIVEN typeof ...prototype THEN returns object', () => {
+        expect(typeof BunnyCdnStream.prototype).toBe('object');
+      });
     });
 
-    test('GIVEN typeof ...prototype THEN returns object', () => {
-      expect(typeof BunnyCdnStream.prototype).toBe('object');
+    describe('is a valid class', () => {
+      stream = new BunnyCdnStream({ videoLibrary: process.env.BUNNY_VIDEO_LIBRARY || '123', apiKey: process.env.BUNNY_API_KEY || '123' });
+      test('GIVEN instanceof BunnyCdnStream THEN returns true', () => {
+        expect(stream instanceof BunnyCdnStream).toBe(true);
+      });
     });
-  });
-
-  jest.setTimeout(100000);
-  describe('is a valid class', () => {
-    stream = new BunnyCdnStream({ videoLibrary: process.env.BUNNY_VIDEO_LIBRARY || '123', apiKey: process.env.BUNNY_API_KEY || '123' });
-    test('GIVEN instanceof BunnyCdnStream THEN returns true', () => {
-      expect(stream instanceof BunnyCdnStream).toBe(true);
-    });
-  });
-});
+  },
+  { timeout: 10000 }
+);
